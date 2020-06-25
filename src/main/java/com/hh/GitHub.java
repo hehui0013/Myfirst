2ef3a2e910aa7a9e1c7635d0ce714a2916d03416
hh0013 <hehui525948@163.com>
2020-06-25 21:45:10 +0800

    text

---
+++ src/main/java/com/hh/GitHub.java
@@ -0,0 +1,12 @@
+package com.hh;
+
+/**
+ * @author: hehui
+ * @date: 2020/6/25
+ * @description:
+ */
+public class GitHub {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
